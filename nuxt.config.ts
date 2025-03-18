51e70cdf81fdd80db78f1538a4d9010f78ba2c28
Nick Efimov <lamoz760@gmail.com>
2025-03-18 09:34:22 +0300

    add main structure

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+export default defineNuxtConfig({
+  runtimeConfig: {
+      // Приватные ключи, доступные только на сервере
+      apiSecret: "123",
+      // Ключи внутри public также доступны и на клиенте
+      public: {
+          apiBase: "/api",
+      },
+  },
+
+  compatibilityDate: "2025-03-18",
+});
